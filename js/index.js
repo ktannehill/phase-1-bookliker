5bd3e6cf3b1c572e90f1a10f377f7af8bed66cac
Kat Tannehill <katetannehill@gmail.com>
2023-09-19 14:51:13 -0600

    fetch books from server

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// document.addEventListener("DOMContentLoaded", function() {});
+// 1. fetch books
+    // display book titles as li in ul#list
+
+// Global varibles
+const BOOKSURL = "http://localhost:3000/books"
+const USERSURL = "http://localhost:3000/users"
+const listUL = document.querySelector("#list")
+
+
+// Helper functions
+const createBookList = (book) => {
+    // console.log(book)
+    // debugger
+
+}
+
+
+// Fetch requests
+fetch(BOOKSURL)
+.then(resp => {
+    if(resp.ok) {
+        return resp.json()
+    } else {
+        throw("Error!")
+    }
+})
+.then(booksData => {
+    booksData.forEach(createBookList)
+})
+.catch(err => alert(err))
